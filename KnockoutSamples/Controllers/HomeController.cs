49f969b975937e53af4c3382a780321808877792
Jordan Terrell <jterrell@wans.net>
2012-05-30 22:48:25 -0500

    Initial import...

---
+++ KnockoutSamples/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+
+namespace KnockoutSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SimpleTextBinding()
+        {
+            return View();
+        }
+
+        public ActionResult PrimitiveValueTextBinding()
+        {
+            return View();
+        }
+
+        public ActionResult ObservableValueTextBinding()
+        {
+            return View();
+        }
+
+        public ActionResult ComputedValueBinding()
+        {
+            return View();
+        }
+
+        public ActionResult VisibleBinding()
+        {
+            return View();
+        }
+
+        public ActionResult CssBinding()
+        {
+            return View();
+        }
+    }
+}
